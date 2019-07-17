a43521e8ce2db7d80937a6db016e622e423ceece
Yara Castro <yaracastro24@gmail.com>
2019-07-17 10:22:10 -0500

    inital commit

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,9 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+const createNewArticle = () =>{
+  const article = document.querySelector('.articles');
+  const newArticle = document.createElement('div');
+  newArticle.classList.add('article');
+  const 
+}
